e04beba2ca63217eed214c6ab245eba8901f8021
davera <danielavelez1094@gmail.com>
2021-08-15 18:32:53 -0500

    Se crea la clase vista

---
+++ src/model/Curso.java
@@ -0,0 +1,39 @@
+package model;
+
+public class Curso {
+    
+    private int codigo;
+    private String nombre;
+    private char jornada;
+
+    public Curso (int pCodigo, String pNombre, char pJornada) {
+        super();
+        this.codigo = pCodigo;
+        this.nombre = pNombre;
+        this.jornada = pJornada;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public char getJornada() {
+        return jornada;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setJornada(char jornada) {
+        this.jornada = jornada;
+    }
+}
